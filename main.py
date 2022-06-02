5836a19e0ad17aaeb6af0d1fedb622c50ff7735c
Egor.Andreev <egor_andreev@unilever.com>
2022-06-02 17:47:58 +0300

    добавлен 2й вопрос про цвет и ответ бота

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name = input("имя?")
-print(name + ", ты балбес")
+name = input("имя?\n")
+print(name + ", ты балбес!")
+color = input("ну а какой твой любимый цвет?\n")
+print(color + " цвет - дерьмовый, " + name + "!")
