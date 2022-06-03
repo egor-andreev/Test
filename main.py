fb8342698ec0b97bb1b8b3e63fa283f49326d741
Egor.Andreev <egor_andreev@unilever.com>
2022-06-03 13:46:22 +0300

    финалочка

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 name = input("имя?\n")
 print(name + ", ты балбес!")
 color = input("ну а какой твой любимый цвет?\n")
-print(color + " цвет - дерьмовый, " + name + "!")
+print(color + " цвет - супер, " + name + "!")
 
 ## uploading to git
-##ok next update##
